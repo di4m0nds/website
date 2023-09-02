6e239bcb83d760ffd2bc0ce66de98fac669f3f16
di4m0nds <ws.2000.sja@gmail.com>
2023-09-02 13:33:10 -0300

    Update design + structure

---
+++ src/utils/functions.ts
@@ -0,0 +1,27 @@
+export const timeSincePublication = (articleDate: Date) : string => {
+  // Get the current date and time
+  const currentDate = new Date();
+
+  // Calculate the time difference in milliseconds
+  const timeDifference = currentDate.getTime() - articleDate.getTime()
+
+  // Convert the time difference to seconds
+  const seconds = Math.floor(timeDifference / 1000);
+
+  // Calculate minutes, hours, and days
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  console.log(minutes, hours, days)
+
+  if (days > 0) {
+    return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+  } else if (hours > 0) {
+    return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+  } else {
+    return 'Just now';
+  }
+}
