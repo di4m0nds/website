b69fa01bce7727a0ddb9aa7ce1071906a50bdf39
di4m0nds <ws.2000.sja@gmail.com>
2023-09-27 11:02:29 -0300

    Bug fixes and enhancements

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -3,17 +3,23 @@ export const timeSincePublication = (articleDate: Date) : string => {
   const currentDate = new Date();
 
   // Calculate the time difference in milliseconds
-  const timeDifference = currentDate.getTime() - articleDate.getTime()
+  const timeDifference = currentDate.getTime() - articleDate.getTime();
 
   // Convert the time difference to seconds
   const seconds = Math.floor(timeDifference / 1000);
 
-  // Calculate minutes, hours, and days
+  // Calculate minutes, hours, days, months, and years
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30); // Assuming an average of 30 days per month
+  const years = Math.floor(months / 12);
 
-  if (days > 0) {
+  if (years > 0) {
+    return `${years} ${years === 1 ? 'year' : 'years'} ago`;
+  } else if (months > 0) {
+    return `${months} ${months === 1 ? 'month' : 'months'} ago`;
+  } else if (days > 0) {
     return `${days} ${days === 1 ? 'day' : 'days'} ago`;
   } else if (hours > 0) {
     return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
