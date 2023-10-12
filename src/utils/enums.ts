9d056e30ff891af65e79ad3c6fe10a711ad3dbb2
di4m0nds <ws.2000.sja@gmail.com>
2023-10-12 12:44:26 -0300

    Implemented Animated Page Transitions and Enhanced Post Design

---
+++ src/utils/enums.ts
@@ -0,0 +1,8 @@
+export enum DateFormat {
+  ISO8601 = 'YYYY-MM-DDTHH:mm:ss.sssZ',
+  ShortDate = 'MM/DD/YYYY',
+  LongDate = 'MMMM DD, YYYY',
+  ShortDateTime = 'MM/DD/YYYY HH:mm',
+  LongDateTime = 'MMMM DD, YYYY HH:mm',
+}
+
