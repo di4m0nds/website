6d917e43b84c493a1621f9f957acf8981409d0e5
di4m0nds <ws.2000.sja@gmail.com>
2024-02-20 10:56:27 -0300

    Add @astrojs/rss package, update dependencies, and create RSS feed endpoint

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,23 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const postsData = await getCollection('posts')
+  const posts = postsData.sort(
+    (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()
+  )
+
+  return rss({
+    title: 'Javi’s Blog',
+    description: 'Posts about topics that I currently learning.',
+    site: 'https://javiersilvestri.vercel.app',
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      tags: `${post.data.tags}`,
+      ready: `${post.data.ready}`,
+      pubDate: post.data.pubDate,
+      link: `/posts/${post.slug}/`,
+    })),
+  });
+}
