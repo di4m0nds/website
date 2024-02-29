069da19cbf1ad0098e1c6fc1af09bb3b2ab904d8
di4m0nds <ws.2000.sja@gmail.com>
2024-02-29 06:30:41 -0300

    feat: Update RSS feed to use URL enum
    - Updated the RSS feed to use the URL enum for better maintainability and readability.

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -5,3 +5,10 @@ export enum DateFormat {
   ShortDateTime = 'MM/DD/YYYY HH:mm',
   LongDateTime = 'MMMM DD, YYYY HH:mm'
 }
+
+export enum URL {
+  BASE_URL = 'https://javiersilvestri.vercel.app',
+  POSTS = '/posts'
+}
+
+export const getPostsUrl = () => `${URL.BASE_URL}${URL.POSTS}`
