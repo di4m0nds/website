c5ba615d93c67b4b4967ac7209dd6b769baff631
Javier Silvestri <ne70459@gmail.com>
2026-04-10 01:24:15 +0000

    feat: cursor, AI about, GitHub stats, glitch fx, console narration, RSS fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,28 @@
-import rss from ‘@astrojs/rss’
-import { getCollection } from ‘astro:content’
-import { siteConfig } from ‘../data/portfolio’
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+import { siteConfig } from '../data/portfolio'
 
 export async function GET(context) {
-  const postsData = await getCollection(‘posts’)
+  const postsData = await getCollection('posts')
   const posts = postsData
     .filter((p) => p.data.ready)
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
 
   return rss({
-    title: `${siteConfig.name}’s Blog`,
-    description: ‘Articles about software development, engineering, and things I’m learning.’,
+    title: siteConfig.name + "'s Blog",
+    description: 'Articles about software development, engineering, and things I am learning.',
     site: context.site ?? siteConfig.url,
     xmlns: {
-      atom: ‘http://www.w3.org/2005/Atom’,
+      atom: 'http://www.w3.org/2005/Atom',
     },
-    customData: `<atom:link href="${siteConfig.url}/rss.xml" rel="self" type="application/rss+xml" />`,
-    items: posts.map((po
+    customData:
+      '<atom:link href="' + siteConfig.url + '/rss.xml" rel="self" type="application/rss+xml" />',
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: '/posts/' + post.slug + '/',
+      categories: post.data.tags,
+    })),
+  })
+}
